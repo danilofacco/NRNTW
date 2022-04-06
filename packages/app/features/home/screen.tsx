35ca991d0b212a7369030dd9d78d07204ad4b54d
Danilo Facco <faccodanilo@gmail.com>
2022-04-06 20:26:40 +0100

    initial commit

---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,74 @@
+import { Text, useSx, View, H1, P, Row, A } from 'dripsy'
+import React from 'react'
+import { TextLink } from 'solito/link'
+import { MotiLink } from 'solito/moti'
+import {useTailwind} from 'tailwind-rn';
+export function HomeScreen() {
+  const tailwind = useTailwind();
+  const sx = useSx()
+
+  return (
+    <View
+    style={tailwind('pt-12 items-center bg-blue-500')}
+      sx={{ flex: 1, justifyContent: 'center', alignItems: 'center', p: 16 }}
+    >
+      <H1 sx={{ fontWeight: '800' }}>Welcome to Solito.</H1>
+      <View sx={{ maxWidth: 600 }}>
+        <P style={tailwind('pt-12 items-center bg-blue-500')} sx={{ textAlign: 'center' }}>
+          Here is a basic starter to show you how you can navigate from one
+          screen to another. This screen uses the same code on Next.js and React
+          Native.
+        </P>
+        <P sx={{ textAlign: 'center' }}>
+          Solito is made by{' '}
+          <A
+            href="https://twitter.com/fernandotherojo"
+            // @ts-expect-error react-native-web only types
+            hrefAttrs={{
+              target: '_blank',
+              rel: 'noreferrer',
+            }}
+            sx={{ color: 'blue' }}
+          >
+            Fernando Rojo
+          </A>
+          .
+        </P>
+      </View>
+      <View sx={{ height: 32 }} />
+      <Row>
+        <TextLink
+          href="/user/fernando"
+          textProps={{
+            style: sx({ fontSize: 16, fontWeight: 'bold', color: 'blue' }),
+          }}
+        >
+          Regular Link
+        </TextLink>
+        <View sx={{ width: 32 }} />
+        <MotiLink
+          href="/user/fernando"
+          animate={({ hovered, pressed }) => {
+            'worklet'
+
+            return {
+              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
+              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
+            }
+          }}
+          transition={{
+            type: 'timing',
+            duration: 150,
+          }}
+        >
+          <Text
+            selectable={false}
+            sx={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
+          >
+            Moti Link
+          </Text>
+        </MotiLink>
+      </Row>
+    </View>
+  )
+}
