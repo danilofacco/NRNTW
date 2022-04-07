36dc6aa6de1c466c041631a36c49e0ab26345eb5
Danilo Facco <faccodanilo@gmail.com>
2022-04-07 09:34:05 +0100

    Update CrossImage.tsx

--- packages/app/features/cross/CrossImage.tsx
+++ packages/app/features/cross/CrossImage.tsx
@@ -1,15 +1,22 @@
 import Image from "next/image"
 import React from "react"
-interface Props{
+import { Image as NativeImage, Platform } from 'react-native';
+interface Props {
     src: string,
-    w:number,
-    h:number,
-    c:string,
+    w: number,
+    h: number,
+    c: string,
 
 }
-const  CrossImage : React.FC<Props> = ({src,w,h,c}:Props) => {
-    
-    return(<Image src={src} width={w} height={h}/>)
+const CrossImage: React.FC<Props> = ({ src, w, h, c }: Props) => {
+
+    return (<>
+        {Platform.OS === "web" ?
+            <Image src={src} width={w} height={h} />
+            :
+            <NativeImage source={{ uri:src}}  width={w} height={h} />
+}
+    </>)
 
 }
 
